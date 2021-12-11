c43e05785caea57104d293ad40b775accb8e5241
Levitsskay <91135524+Levitsskay@users.noreply.github.com>
2021-12-11 19:17:05 +0200

    Add files via upload

---
+++ lab1/lab4/js/java.js
@@ -0,0 +1,7 @@
+function CheckData() 
+{
+var ans;
+ans = confirm("Вы уверены, что хотите отправить введенные данные ?");
+if (ans) submit(); 
+}
+'aaa aBa aca aDa afa aZa aga '.replace(/a[a-fA-Z]a/, '!');
